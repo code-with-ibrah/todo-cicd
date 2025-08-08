9b5e580959ddfff05c79ad5b35d8389aec7c4757
= <ibrahimyakubu026860@gmail.com>
2025-08-08 00:53:46 -0700

    removed store folder

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = 3500;
+
+
+app.get("/", (req, res) => {
+
+
+    return res.send("asdfsad");
+})
+
+
+
+
+
+
 
 app.listen(port, () => {console.log("Server is running on port: http://localhost:" + port)});
